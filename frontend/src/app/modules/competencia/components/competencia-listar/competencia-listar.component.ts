115a07988ad9f026ed4ce59e532493d7919b0c9d
Jeronimo Espala <jeronimo_espala@hotmail.com>
2022-04-05 08:26:39 -0300

    Comecando Frontend

---
+++ frontend/src/app/modules/competencia/components/competencia-listar/competencia-listar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competencia-listar',
+  templateUrl: './competencia-listar.component.html',
+  styleUrls: ['./competencia-listar.component.css']
+})
+export class CompetenciaListarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
