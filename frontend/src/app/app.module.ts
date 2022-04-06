ba32759dc0811d4dd029af0de464e26b2aaf5ab5
basis <jeronimo_espala@hotmail.com>
2022-04-06 13:20:18 -0300

    Ajustando o App

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { PageNotificationModule, BreadcrumbModule, MenuModule, ErrorStackModule 
 import { ErrorModule, SecurityModule, VersionTagModule } from '@nuvem/angular-base';
 import { DiarioErrosComponent } from './components/diario-erros/diario-erros.component';
 import { BlockUIModule } from 'ng-block-ui';
-import { CompetenciaComponent } from './modules/competencia/competencia/competencia.component';
 import { CompetenciaListarComponent } from './modules/competencia/components/competencia-listar/competencia-listar.component';
 
 @NgModule({
@@ -22,7 +21,6 @@ import { CompetenciaListarComponent } from './modules/competencia/components/com
         AppTopbarComponent,
         AppFooterComponent,
         DiarioErrosComponent,
-        CompetenciaComponent,
         CompetenciaListarComponent
     ],
     imports: [
